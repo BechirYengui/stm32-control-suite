1af3ca5208b355407668574d5fe04f2380896c8f
Bechir Yengui <146326821+BechirYengui@users.noreply.github.com>
2026-05-15 21:37:25 +0200

    add_PWM_clamp

--- desktop/tests/test_devicestate.cpp
+++ desktop/tests/test_devicestate.cpp
@@ -9,6 +9,8 @@ class TestDeviceState : public QObject
 private slots:
     void testDefaults();
     void testTemperatureThreshold();
+    void testPwmClamp();
+    void testFormattedUptime();
 };
 
 void TestDeviceState::testDefaults()
@@ -30,6 +32,29 @@ void TestDeviceState::testTemperatureThreshold()
     s.setTemperature(30.0f);
 
     QCOMPARE(spy.count(), 2);
+}
+
+void TestDeviceState::testPwmClamp()
+{
+    DeviceState s;
+    s.setPwmDutyCycle(200); // au dessus du max, doit etre clamp a 100
+    QCOMPARE(s.pwmDutyCycle(), static_cast<uint8_t>(DeviceState::kMaxPwmDutyCycle));
+
+    s.setPwmDutyCycle(50);
+    QCOMPARE(s.pwmDutyCycle(), static_cast<uint8_t>(50));
+}
+
+void TestDeviceState::testFormattedUptime()
+{
+    DeviceState s;
+    s.setUptime(0);
+    QCOMPARE(s.formattedUptime(), QString("00:00:00"));
+
+    s.setUptime(65); // 1 min 5 s
+    QCOMPARE(s.formattedUptime(), QString("00:01:05"));
+
+    s.setUptime(3661); // 1h 1min 1s
+    QCOMPARE(s.formattedUptime(), QString("01:01:01"));
 }
 
 QTEST_GUILESS_MAIN(TestDeviceState)
